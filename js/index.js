296d53ccc20e426e34b57948d8e28f22501986be
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-02-23 14:00:01 +0500

    Updated File

--- js/index.js
+++ js/index.js
@@ -86,8 +86,6 @@ const filterInput = (arr, htmlElement) => {
     arr.forEach(pokemon => {
         if (pokemon.name.includes(inputValue)) {
             filteredPokemonsByName.push(pokemon)
-        }else{
-            console.log(`${inputValue} nomli pokemon mavjud emas`);
         }
     })
     localStorage.setItem('filterpokemons', JSON.stringify(filteredPokemonsByName))
